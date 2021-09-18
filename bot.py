ab16d39e3fd7e2faa9f2d88ee38e23af3543b4ae
sarim2000 <sarimbleedblue@gmail.com>
2021-09-19 00:47:54 +0530

    mb

--- bot.py
+++ bot.py
@@ -28,7 +28,7 @@ def datetime_from_utc_to_local(utc_datetime):
 
 def utc2local(utc):
     print(utc)
-    return arrow.get(utc).to(('Asia/Kolkata')).format('%A %d. %B %Y HH:mm:ss ZZ')
+    return arrow.get(utc).to(('Asia/Kolkata')).format('HH:mm:ss ZZ')
 
 
 def codeforces(update, context):
@@ -39,10 +39,11 @@ def codeforces(update, context):
         start = x["start_time"]
         end = x["end_time"]
         d1 = datetime_from_utc_to_local(start)
+        print(d1.strftime("%A %d. %B %Y"))
         d4 = utc2local(start)
         d2 = utc2local(end)
         new_format = "%Y-%m-%d"
-        info += (x["name"]+"\nStart: "+str(d4) +
+        info += (x["name"]+"\nStart: "+str(d1) + " " + str(d4) +
                  "\nEnd: "+str(d2)+"\nRegister: "+x["url"] + "\n\n")
     update.message.reply_text(info)
 
